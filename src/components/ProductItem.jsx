11b5bd49d979e6a6722f882491144ca3eece34f0
ramsceoskull <ramses.social0@gmail.com>
2023-02-03 14:13:28 -0600

    Toogle del menú

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,10 +4,10 @@ import '@styles/ProductItem.scss';
 import addToCart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
-  const [cart, setCart] = useState('Hola');
+  const [cart, setCart] = useState([]);
 
   const handleClick = () => {
-    setCart('Hola Mundo');
+    setCart([]);
   }
 
 	return (
@@ -21,7 +21,6 @@ const ProductItem = () => {
 				<figure onClick={handleClick}>
 					<img src={addToCart} alt="add to cart" />
 				</figure>
-        {cart}
 			</div>
 		</div>
 	);
