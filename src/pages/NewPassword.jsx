f92ae58879e39e82ede5361d1e5106901ddcae1d
ramsceoskull <ramses.social0@gmail.com>
2023-02-03 00:02:45 -0600

    Componentes y rutas de React Shop

--- src/pages/NewPassword.jsx
+++ src/pages/NewPassword.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import '../styles/Login.scss';
+import '../styles/NewPassword.scss';
 
-const Login = () => {
+const NewPassword = () => {
   return (
-    <div className="login">
-      <div className="form-container">
+    <div className="NewPassword">
+      <div className="NewPassword-container">
         <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
         <h1 className="title">Create a new password</h1>
-        <p className="subtitle">Enter a new passwrd for yue account</p>
+        <p className="subtitle">Enter a new password for your account</p>
         <form action="/" className="form">
-          <label for="password" className="label">Password</label>
+          <label htmlFor="password" className="label">Password</label>
           <input type="password" id="password" placeholder="*********" className="input input-password" />
-          <label for="new-password" className="label">Password</label>
+          <label htmlFor="new-password" className="label">Password</label>
           <input type="password" id="new-password" placeholder="*********" className="input input-password" />
           <input type="submit" value="Confirm" className="primary-button login-button" />
         </form>
@@ -20,4 +20,4 @@ const Login = () => {
   );
 }
 
-export default Login;
+export default NewPassword;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/NotFound.css'
+import '../styles/NotFound.scss'
 
 const NotFound = () => {
   return (
