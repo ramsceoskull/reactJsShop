150411d51b13d74ae469585b3ce86a0ea6b484f2
ramsceoskull <ramses.social0@gmail.com>
2023-02-03 17:49:31 -0600

    useRef y formularios

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,13 +6,13 @@ const Menu = () => {
 		<div className="Menu">
 			<ul>
 				<li>
-					<a href="/" className="title">My orders</a>
+					<a href="/orders" className="title">My orders</a>
 				</li>
 				<li>
-					<a href="/">My account</a>
+					<a href="/account">My account</a>
 				</li>
 				<li>
-					<a href="/">Sign out</a>
+					<a href="/login">Sign out</a>
 				</li>
 			</ul>
 		</div>
