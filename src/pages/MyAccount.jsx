f21e228bb17983f24aed56fc3cd2451bc9394583
ramsceoskull <ramses.social0@gmail.com>
2023-02-03 13:18:39 -0600

    Imagenes y alias en Webpack

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/MyAccount.scss';
+import '@styles/MyAccount.scss';
 
 const MyAccount = () => {
 	return (
@@ -8,11 +8,11 @@ const MyAccount = () => {
 				<h1 className="title">My account</h1>
 				<form action="/" className="form">
 					<div>
-						<label for="name" className="label">Name</label>
+						<label htmlFor="name" className="label">Name</label>
 						<p className="value">Camila Yokoo</p>
-						<label for="email" className="label">Email</label>
+						<label htmlFor="email" className="label">Email</label>
 						<p className="value">camilayokoo@gmail.com</p>
-						<label for="password" className="label">Password</label>
+						<label htmlFor="password" className="label">Password</label>
 						<p className="value">*********</p>
 					</div>
 					<input type="submit" value="Edit" className="secondary-button login-button" />
