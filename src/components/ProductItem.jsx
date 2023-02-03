665986cda0c17814f996056f19c2752355062637
ramsceoskull <ramses.social0@gmail.com>
2023-02-03 13:44:20 -0600

    React.useState}

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 
 import addToCart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+  const [cart, setCart] = useState('Hola');
+
+  const handleClick = () => {
+    setCart('Hola Mundo');
+  }
+
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -12,9 +18,10 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleClick}>
 					<img src={addToCart} alt="add to cart" />
 				</figure>
+        {cart}
 			</div>
 		</div>
 	);
